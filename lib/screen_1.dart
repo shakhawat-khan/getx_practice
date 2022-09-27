6ec3a1fcf091f1e03e12f36cb65e72cf3aedb9b3
shakhawat-khan <satanfromhelll666@gmail.com>
2022-09-27 18:45:23 +0600

    bottom_sheet

--- lib/screen_1.dart
+++ lib/screen_1.dart
@@ -34,6 +34,30 @@ class Screen1 extends StatelessWidget {
             );
 
             }, child: const Text('screen 2')),
+
+          ElevatedButton(onPressed: (){
+            Get.bottomSheet(
+              Container(
+
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    ListTile(title: Text("Option 1"),
+                      trailing: Icon(Icons.access_alarms),),
+                    ListTile(title: Text("Option 2"),
+                      trailing: Icon(Icons.ac_unit),),
+                    ListTile(title: Text("Option 3"),
+                      trailing: Icon(Icons.present_to_all_sharp),),
+                    ListTile(title: Text("Option 4"),
+                      trailing: Icon(Icons.keyboard),),
+                  ],
+                ),
+              ),
+              isDismissible: false,
+              enableDrag: true,
+
+            );
+          }, child: Text('bottom sheet text')),
         ],
       )
 
